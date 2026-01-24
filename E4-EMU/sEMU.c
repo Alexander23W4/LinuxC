17662e7fd42bd6840a2a37c00255f1ef7490df31
UB22.04Wang <2842wang6843w@gmail.com>
2026-01-24 22:03:42 +0800

    create sEMU, successful

---
+++ E4-EMU/sEMU.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdint.h>
+/*
+sISA
+
+The opcode : add li bner0 
+ 7  6 5  4 3   2 1   0
++----+----+-----+-----+
+| 00 | rd | rs1 | rs2 | R[rd]=R[rs1]+R[rs2]      ADD instruction for register addition
++----+----+-----+-----+
+| 10 | rd |    imm    | R[rd]=imm               LI instruction (Load Immediate with zero-extension)
++----+----+-----+-----+
+| 11 |   addr   | rs2 | if (R[0]!=R[rs2]) PC=addr BNER0 instruction (Branch if Not Equal to Register 0)
++----+----------+-----+
+
+*/
+uint8_t pc = 0;
+uint8_t GPR[4];
+uint8_t M[16];
+/*
+Fill Momery with 
+0: li r0, 10   # This is decimal 10.
+1: li r1, 0
+2: li r2, 0
+3: li r3, 1
+4: add r1, r1, r3
+5: add r2, r2, r1
+6: bner0 4, r1
+7: bner0 7, r3
+*/
+void _v_fill_memory(void)
+{
+    M[0] = 0b10001010;
+    M[1] = 0b10010000;
+    M[2] = 0b10100000;
+    M[3] = 0b10110001;
+    M[4] = 0b00010111;
+    M[5] = 0b00101001;
+    M[6] = 0b11010001;
+    M[7] = 0b11011111;
+}
+uint8_t operate(void);
+uint8_t next_pc;
+int main(int argc, char** argv)
+{
+    _v_fill_memory();
+    while(pc != 7){
+        next_pc = operate();
+        if(pc != 99){
+            pc = next_pc;
+        }
+        else{
+            printf("Error\n");
+            break;
+        }
+        printf("Result: %d\n", GPR[2]);
+    }
+}
+
+uint8_t operate(void){
+    uint8_t next = 0;
+    uint8_t code = M[pc];    // Fetch is already downed
+    // Decode, Execute, Update_PC: 
+    switch(code >> 6){
+        case 0b00:
+            GPR[(code & 0b00110000) >> 4] = GPR[(code & 0b00001100) >> 2] + GPR[(code & 0b00000011)];
+            next = pc + 1;
+            break;
+        case 0b10:
+            GPR[(code & 0b00110000) >> 4] = (code & 0b00001111);
+            next = pc + 1;
+            break;
+        case 0b11:
+            next = (GPR[(code & 0b00000011)] == GPR[0]) ? (pc + 1) : ((code & 0b00111100) >> 2);
+            break;
+        default:
+            next = 99;
+            break;     
+    }
+    return next;
+}
+
+
+
+
+
