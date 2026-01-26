78cce2150a421653e1eb88fb004714c871fa3853
UB22.04Wang <2842wang6843w@gmail.com>
2026-01-26 16:12:38 +0800

    update miniEMU 26-1-26

--- E4-EMU/mini-EMU/miniEMU.c
+++ E4-EMU/mini-EMU/miniEMU.c
@@ -3,7 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define GPR_MASK 0x1F
+#define _GPR_MASK 0x1F
+#define OPERATING_CIRCLE_TERMINATION__MEM 6000
+#define OPERATING_CIRCLE_TERMINATION__SUM 6000
+#define GPR_AMOUNT 32
+#define GENERAL_BIT_WIDTH 32
+#define MEMORY_AMOUNT 2048   // 4KB
+#define MEMORY_LOAD_EFFECTIVENESS 2000
 
 /*
 mini-RV ISA   8 in total:
@@ -19,35 +25,65 @@ imm[11:5] rs2 rs1 000 imm[4:0] 0100011 SB
 
 */
 int operate(void);
+void output_elements(int circle);
+void load_memory(char* filename);
 
-int pc = 0;
-int GPR[32] = {0};
-int M[1024] = {0};
+uint32_t pc = 0;
+uint32_t GPR[GPR_AMOUNT] = {0};
+uint32_t M[MEMORY_AMOUNT] = {0};
+
+int _operating_circles = 0;
 
 int main(int argc, char** argv){
     // load the codes into memory
+    assert(argc >= 2);
+    load_memory(argv[1]);
 
     // operate until the very end
-    
+    while(_operating_circles <= OPERATING_CIRCLE_TERMINATION__MEM){
+        int next = operate();
+        assert(next != -1);
+        pc = next;
+        output_elements(_operating_circles);         // print all variants (GPRs) for each loop
+        _operating_circles++;
+    }
+}
+void load_memory(char* filename){   
+    FILE *fp = fopen(filename, "rb");  
+    assert(fp);
+// get instr amount
+    fseek(fp, 0, SEEK_END);
+    long total_bytes = ftell(fp);
+    fseek(fp, 0, SEEK_SET);  
+    size_t loaded_instr = fread(M, sizeof(uint32_t), MEMORY_LOAD_EFFECTIVENESS, fp);
+    fclose(fp);
+
+    printf("--LOAD %zu AMOUNTS OF INSTR TO M[]\n", loaded_instr);
 }
 
-int32_t sign_extend_s_type(uint32_t code) {
+void output_elements(int circle){     // &&&
+    printf("---%d---", circle);
+    for(int i = 0; i < GPR_AMOUNT; i++){
+        printf("GPR %d: %d\n", i, GPR[i]);
+    }
+    printf("\n\n");
+}
+
+int32_t sign_extend_s_type(uint32_t code) {   // &&&
     int imm11_5 = (code >> 25) & 0x7F;    // bit[31:25]
-    int imm4_0 = (code >> 7) & 0x1F;      // bit[11:7]  
-    int imm11 = (code >> 31) & 1;         
+    int imm4_0 = (code >> 7) & 0x1F;      // bit[11:7]      
     
-    return (imm11 << 11) | (imm11_5 << 5) | (imm4_0);  
+    return (imm11_5 << 5) | (imm4_0);  
 }
 
 
-int32_t sign_extend(uint32_t instr, int bit_width) {
+int32_t sign_extend(uint32_t instr, int bit_width) {    // &&&
 
     uint32_t field = (instr >> (32 - bit_width)) & ((1 << bit_width) - 1);
     
     if (field & (1 << (bit_width - 1))) {
         return field | (~0U << bit_width);
     } else {
-
         return field;
     }
 }
@@ -58,43 +94,47 @@ int operate(void){
     int opcode = code & 0x7F;
     int opcode__a = (code & 0x7000) >> 12;
     int next = 0;
+    int32_t imm;
+    int32_t rs1;
+    int32_t rd;
+    int32_t rs2;
     switch(opcode){
         case 0b1100111:   // jalr   jump and link 
-            int32_t imm = sign_extened(code >> 20, 12);
-            int32_t rs = (code >> 15) & 0x1F;
-            int32_t rd = (code >> 7) & 0x1F;
+            imm = sign_extend(code >> 20, 12);
+            rs1 = (code >> 15) & 0x1F;
+            rd = (code >> 7) & 0x1F;
             if(rd != 0) GPR[rd] = pc + 4;
-            next = (imm + GPR[rs]) & ~1;    // ~1 is reversed code of 1(0000....0001)
+            next = (imm + GPR[rs1]) & ~1;    // ~1 is reversed code of 1(0000....0001)
             break;
 
         case 0b0010011:   // addi    // add immediate
-            int32_t imm = sign_extened(code >> 20, 12);
-            int32_t rs = (code >> 15) & 0x1F;
-            int32_t rd = (code >> 7) & 0x1F;
-            GPR[rd] = GPR[rs] + imm;
+            imm = sign_extend(code >> 20, 12);
+            rs1 = (code >> 15) & 0x1F;
+            rd = (code >> 7) & 0x1F;
+            GPR[rd] = GPR[rs1] + imm;
             next = pc + 4;
             break;
 
         case 0b0110011:   // add    // add register-register
-            int32_t rs1 = (code >> 20) & 0x1F;
-            int32_t rs2 = (code >> 15) & 0x1F;
-            int32_t rd = (code >> 7) & 0x1F;
+            rs1 = (code >> 20) & 0x1F;
+            rs2 = (code >> 15) & 0x1F;
+            rd = (code >> 7) & 0x1F;
             GPR[rd] = GPR[rs1] + GPR[rs2];
             next = pc + 4;
             break;
 
         case 0b0110111:   // lui (load upper immediate)
-            int32_t imm = sign_extend((code >> 12) << 12, 32);
-            int32_t rd = (code >> 7) & 0x1F;
+            imm = (code >> 12) << 12;
+            rd = (code >> 7) & 0x1F;
             GPR[rd] = imm;
             next = pc +4;
             break;
 
         case 0b0000011:   // lw | lbu     // pull data from RAM to GPR
             assert(opcode__a == 0b010 || opcode__a == 0b100);
-            int rs1 = (code >> 15) & 0x1F;
-            int rd = (code >> 7) & 0x1F;
-            int32_t imm = sign_extend(code >> 20, 12);
+            rs1 = (code >> 15) & 0x1F;
+            rd = (code >> 7) & 0x1F;
+            imm = sign_extend(code >> 20, 12);
             if(opcode__a == 0b010){ // lw
                 GPR[rd] = M[(GPR[rs1] + imm) >> 2];
             }
@@ -115,8 +155,8 @@ int operate(void){
         case 0b0100011:   // sw | sb
             assert(opcode__a == 0b010 || opcode__a == 0b000);
             int funct3 = (code >> 12) & 0x7;
-            int rs1 = (code >> 15) & 0x1F;
-            int rs2 = (code >> 20) & 0x1F;  
+            rs1 = (code >> 15) & 0x1F;
+            rs2 = (code >> 20) & 0x1F;  
             int32_t imm = sign_extend_s_type(code);  
             
             uint32_t addr = GPR[rs1] + imm;
