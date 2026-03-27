4619f6c332d420bf51d3fa2bb1d0243c3028a67a
UB22.04Wang <2842wang6843w@gmail.com>
2026-03-27 16:48:11 +0800

    Fri Mar 27 16:48:11 CST 2026

--- Wizard_force_C/C19/c19.c
+++ Wizard_force_C/C19/c19.c
@@ -40,7 +40,6 @@ int main(int argc, char *argv[])
 
     printf("What's your First Name? ");
     in = fgets(you.first_name, MAX_DATA-1, stdin);
-    in = fgets(you.first_name, MAX_DATA-1, stdin);
     check(in != NULL, "Failed to read first name.");  // check
 
     printf("What's your Last Name? ");
@@ -82,7 +81,105 @@ error:
     return -1;
 }
 
+
+appendix_1(){
+
+    Person bar = {.last_name = "Jordan"};
+    char* in;
+
+    printf("What's your First Name? ");
+    in = fgets(bar.first_name, MAX_DATA-1, stdin);
+    check(in != NULL, "Failed to read first name.");  // check
+
+    printf("Your age:");
+    char* age;
+    in = fgets(age, MAX_DATA - 1, stdin);
+    // check
+
+    bar.age = atoi(age);
+
+    scanf("%d", &bar.income);
+    // check
+
+error:
+
+}
+
+appendix_2(){
+    // make input name stored without blank and \n
+
+
+}
+
+char* optimize_name(char* name){
+    char* temp = name;
+    char* buffer = (char*)malloc(sizeof(char) * MAX_DATA);
+    int n = 0;
+    while(*temp != '\0'){
+        if(*temp != '\n' && *temp != ' ') {
+            buffer[n] = (*temp);
+            n++;
+        }
+        temp++;
+    }
+    buffer[n] = '\0';
+    return buffer;
+}
+
+/*
+使用scanf编写函数，按照文件名读取文件内容，每次读取单个字符，但是不要越过（文件和缓冲区的）末尾。
+使这个函数接受字符串大小来更加通用，并且确保无论什么情况下字符串都以'\0'结尾。
+*/
+
+void get_content(char* file_name, Person* person){
+    FILE* f = fopen(file_name, "r");
+    assert(f);
+
+    char* buffer;
+    char* buffer_pointer;
+    int read_success;
+    while(read_success = fscanf(f, "%c", buffer_pointer) != EOF){
+        assert(read_success);
+        buffer_pointer++;
+    }
+    *buffer_pointer = '\0';
+
+    /// case..
+    // e.g. case income:
+    person->income = atoi(buffer);
+}
+
+
+// void get_content(char* file_name){
+//     FILE* f = fopen(file_name, "r");
+//     assert(f);
+
+//     char* buffer;
+//     char* buffer_pointer;
+
+//     assert(scanf("%c", buffer_pointer));
+    
+// }
+
+
+
 // #ifndef A
 // #define _concat(A,B) (#A)(#B)A##B
 // #endif
 
+/*
+fscanf
+fgets
+fopen
+freopen
+fdopen
+fclose
+fcloseall
+fgetpos
+fseek
+ftell
+rewind
+fprintf
+fwrite
+fread
+*/
