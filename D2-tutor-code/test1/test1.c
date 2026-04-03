ef1024b31c0b3c79fb6fd1e946dfb5a8cd9aebba
UB22.04Wang <2842wang6843w@gmail.com>
2026-04-03 17:21:25 +0800

    Fri Apr  3 17:21:25 CST 2026

---
+++ D2-tutor-code/test1/test1.c
@@ -0,0 +1,42 @@
+int f(){
+    return 0x123;
+}
+
+int g(){
+    return -123;
+}
+
+int h(){
+    return 0b101010;
+}
+
+// riscv64-unknown-elf-gcc -march=rv32i   -mabi=ilp32   -O2 -c test1.c
+// riscv64-unknown-elf-objdump -d -M no-aliases test1.o
+
+
+/*
+dive deep into how c program compiled, use what kind of instr or instr_combo
+I already know that compiler should do:
+Compilation (3 checking, intermediate language -> optimization) -> assembly(.s) -> .o(binary)
+Linking
+
+test.c (源代码)
+   ↓ 预处理 (preprocessing)
+test.i (预处理后的代码)
+   ↓ 编译 (compilation)
+test.s (汇编语言文件，文本格式)  ← **这才是汇编文件**
+   ↓ 汇编 (assembly)
+test.o (目标文件，二进制机器码)  ← **不是汇编文件**
+   ↓ 链接 (linking)
+test (可执行文件)
+
+I could use objdump push .o file (not linked) back to assembly,
+ check which specific instr compiler use for explain this c program
+
+------------------------------------
+Instr level arrangement:
+
+-> How to load 32 bits constant
+ 
+
+*/
